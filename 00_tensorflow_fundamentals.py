aa160254f1f0246a40d113a10823328d558af0f7
IndraP24 <indrashis985@gmail.com>
2021-07-05 21:56:58 +0530

    Initial fundamentals file

---
+++ 00_tensorflow_fundamentals.py
@@ -0,0 +1,14 @@
+'''
+## Here I covered some of the most fundamental concepts of tensors using TensorFlow
+
+More specifically:
+* Intro to tensors
+* Getting info about tensors
+* Manipulating tensors
+* Tensors & NumPy
+* Using @tf.function (a way to speed up regular Python functions)
+* using GPUs with TensorFlow
+* Some Exercises
+
+'''
+
