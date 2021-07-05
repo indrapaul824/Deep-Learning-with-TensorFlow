77d9ccd83b3e5148dd32e61df63e29ff14aa04eb
IndraP24 <indrashis985@gmail.com>
2021-07-05 22:28:54 +0530

    Add creating tensors using tf.Variable()

--- 00_tensorflow_fundamentals.py
+++ 00_tensorflow_fundamentals.py
@@ -13,6 +13,9 @@ More specifically:
 '''
 
 ''' # Intro to tensors '''
+
+''' ## Using tf.constant() '''
+
 #%% Import TensorFlow
 import tensorflow as tf
 print(tf.__version__)
@@ -20,13 +23,17 @@ print(tf.__version__)
 # %% Create tensors with tf.constant()
 scalar = tf.constant(7)
 scalar
+
 # %% Check the ndims of a tensor
 scalar.ndim
+
 # %% Create a vector
 vector = tf.constant([10, 10])
 vector
+
 # %% Check the ndims of a vector
 vector.ndim
+
 # %% Create a matrix
 matrix = tf.constant([[1, 7],
                       [7, 10]])
@@ -39,8 +46,10 @@ matrix1 = tf.constant([[10., 7.],
                         [3., 2.],
                         [8., 9.]], dtype=tf.float16) # Specifying dtype
 matrix1
+
 # %% Check the ndims of a matrix1
 matrix1.ndim
+
 # %% Create a tensor
 tensor = tf.constant([[[1, 2, 3],
                        [4, 5, 6]],
@@ -49,6 +58,26 @@ tensor = tf.constant([[[1, 2, 3],
                       [[13, 14, 15],
                        [16, 17, 18]]])
 tensor
+
 # %% Check the ndims of tensor
 tensor.ndim
+
+
+''' ## Using `tf.Variable()` '''
+
+# %% Create same tensor with tf.Variable()
+changeable_tensor = tf.Variable([10, 7])
+unchangeable_tensor = tf.constant([10, 7])
+changeable_tensor, unchangeable_tensor
+
+# %% Changing elements in changeable tensors
+changeable_tensor[0].assign(7)
+changeable_tensor
+
+# %% Changing elements in unchangeable tensors (Not Possible)
+unchangeable_tensor[0].assign(7)
+unchangeable_tensor
+
+
+''' ## Creating Random tensors '''
 # %% 
